8665b1c2160ea4ac6162c40d1d6e28e2059b7c7b
Tu nombre <81984791+hannia24@users.noreply.github.com>
2022-02-10 12:30:12 -0500

    commit on master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./paginas/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'accion',
+    loadChildren: () => import('./paginas/accion/accion.module').then( m => m.AccionPageModule)
+  },
+  {
+    path: 'alerta',
+    loadChildren: () => import('./paginas/alerta/alerta.module').then( m => m.AlertaPageModule)
   },
 ];
 
